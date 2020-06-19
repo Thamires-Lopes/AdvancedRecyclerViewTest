2e807eb1975bb202201b1d5f589f569654208257
Thamires Lopes <thamiresl686@gmail.com>
2020-06-19 10:46:53 -0300

    Cleaning code

--- app/src/main/java/com/example/advancedrecyclerviewtest/bottomsheet/BottomSheetFragment.kt
+++ app/src/main/java/com/example/advancedrecyclerviewtest/bottomsheet/BottomSheetFragment.kt
@@ -5,25 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.Nullable
 import com.example.advancedrecyclerviewtest.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 class BottomSheetFragment: BottomSheetDialogFragment() {
 
-    fun newInstance(): BottomSheetFragment? {
-        return BottomSheetFragment()
-    }
     override fun getTheme(): Int {
         return R.style.Theme_MaterialComponents_Light_BottomSheetDialog
     }
 
-    @Nullable
+
     override fun onCreateView(
         inflater: LayoutInflater,
-        @Nullable container: ViewGroup?,
-        @Nullable savedInstanceState: Bundle?
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_bottom_sheet_content, container, false)
     }
